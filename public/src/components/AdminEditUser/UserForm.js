7c83511de33023c7eb2b8491d0d9a6340bc85d08
viveloper <usik1234@gmail.com>
2020-10-15 23:07:40 +0900

    UserForm & ProductForm error validation

--- public/src/components/AdminEditUser/UserForm.js
+++ public/src/components/AdminEditUser/UserForm.js
@@ -1,8 +1,17 @@
-import {Component, renderComponent} from '../../modules/MyReact.js';
+import {Component } from '../../modules/MyReact.js';
+import { validateEmail } from '../../modules/inputValidator.js';
 
 class UserForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputs: {
+        name: props.data.name,
+        email: props.data.email,
+        isAdmin: props.data.isAdmin,
+      },
+      error: '',
+    }
     this.container = document.createElement('form');
     this.container.addEventListener('submit', this.handleSubmit);
   }
@@ -13,26 +22,53 @@ class UserForm extends Component {
     const email = e.target.querySelector('input[id=email]').value;
     const isAdmin = e.target.querySelector('input[id=isadmin]').checked;
 
+    if (!name) {
+      this.setState({
+        inputs: { name, email, isAdmin},
+        error: 'Name is requried',
+      });
+      return;
+    }
+    if (!email) {      
+      this.setState({
+        inputs: { name, email, isAdmin},
+        error: 'Email is requried',
+      });
+      return;
+    }
+    if (!validateEmail(email)) {      
+      this.setState({
+        inputs: { name, email, isAdmin},
+        error: 'Email is not valid',
+      });
+      return;
+    }
+
     this.props.onSubmit({ name, email, isAdmin });
   }
 
-  render() {
-    const { data } = this.props;
+  render() {    
+    const { inputs, error } = this.state;
 
     this.container.innerHTML = `
       <div class="form-group">
         <label class="form-label" for="name">Name</label>
-        <input placeholder="Enter name" type="name" id="name" class="form-control" value="${data.name}">
+        <input placeholder="Enter name" type="name" id="name" class="form-control" value="${inputs.name}">
       </div>
       <div class="form-group">
         <label class="form-label" for="email">Email Address</label>
-        <input placeholder="Enter email" type="email" id="email" class="form-control" value="${data.email}">
+        <input placeholder="Enter email" type="email" id="email" class="form-control" value="${inputs.email}">
       </div>
       <div class="form-group">
         <div class="form-check">
-          <input type="checkbox" id="isadmin" class="form-check-input"${data.isAdmin ? ' checked' : ''}>
+          <input type="checkbox" id="isadmin" class="form-check-input"${inputs.isAdmin ? ' checked' : ''}>
           <label title="" for="isadmin" class="form-check-label">Is Admin</label>
         </div>
+      </div>
+      <div class="text-danger my-3 px-2" style="display:${
+        error ? 'block' : 'none'
+      }">
+        ${error ? error : ''}
       </div>
       <button type="submit" class="btn btn-primary">Update</button>
     `;
--- public/src/components/ProductForm.js
+++ public/src/components/ProductForm.js
@@ -3,6 +3,14 @@ import { Component } from '../modules/MyReact.js';
 class ProductForm extends Component {
   constructor(props) {
     super(props);
+
+    const { name, price, image, brand, countInStock, category, description } = props.data;
+
+    this.state = {
+      inputs: { name, price, image, brand, countInStock, category, description },
+      error: '',
+    }
+
     this.container = document.createElement('form');
     this.container.addEventListener('submit', this.handleSubmit);
     this.container.addEventListener('input', this.handleInput);
@@ -10,8 +18,57 @@ class ProductForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const product = this.getInputs();
-    this.props.onSubmit(product);
+    const inputs = this.getInputs();
+    if (!inputs.name) {
+      this.setState({
+        inputs,
+        error: 'Name is requried',
+      });
+      return;
+    }
+    if (!inputs.price) {      
+      this.setState({
+        inputs,
+        error: 'Price is requried',
+      });
+      return;
+    }
+    if (!inputs.image) {
+      this.setState({
+        inputs,
+        error: 'Image is requried',
+      });
+      return;
+    }
+    if (!inputs.brand) {      
+      this.setState({
+        inputs,
+        error: 'Brand is requried',
+      });
+      return;
+    }
+    if (!inputs.countInStock) {      
+      this.setState({
+        inputs,
+        error: 'Count In Stock is requried',
+      });
+      return;
+    }
+    if (!inputs.category) {      
+      this.setState({
+        inputs,
+        error: 'Category is requried',
+      });
+      return;
+    }
+    if (!inputs.description) {      
+      this.setState({
+        inputs,
+        error: 'Description is requried',
+      });
+      return;
+    }    
+    this.props.onSubmit(inputs);
   }
 
   handleInput = (e) => {
@@ -36,21 +93,21 @@ class ProductForm extends Component {
     return { name, price, image, brand, countInStock, category, description };
   }
 
-  render() {
-    const { data } = this.props;
+  render() {    
+    const { inputs, error } = this.state;
 
     this.container.innerHTML = `
       <div class="form-group">
         <label class="form-label" for="name">Name</label>
-        <input placeholder="Enter name" type="name" id="name" class="form-control" value="${data.name}">
+        <input placeholder="Enter name" type="name" id="name" class="form-control" value="${inputs.name}">
       </div>
       <div class="form-group">
         <label class="form-label" for="price">Price</label>
-        <input placeholder="Enter price" type="number" id="price" class="form-control" value="${data.price}">
+        <input placeholder="Enter price" type="number" id="price" class="form-control" value="${inputs.price}">
       </div>
       <div class="form-group">
         <label class="form-label" for="image">Image</label>
-        <input placeholder="Enter image url" type="text" id="image" class="form-control" value="${data.image}">
+        <input placeholder="Enter image url" type="text" id="image" class="form-control" value="${inputs.image}">
         <div class="custom custom-file">
           <input id="image-file" type="file" class="custom-file-input">
           <label for="image-file" class="custom-file-label">Choose File</label>
@@ -58,19 +115,24 @@ class ProductForm extends Component {
       </div>
       <div class="form-group">
         <label class="form-label" for="brand">Brand</label>
-        <input placeholder="Enter brand" type="text" id="brand" class="form-control" value="${data.brand}">
+        <input placeholder="Enter brand" type="text" id="brand" class="form-control" value="${inputs.brand}">
       </div>
       <div class="form-group">
         <label class="form-label" for="countInStock">Count In Stock</label>
-        <input placeholder="Enter countInStock" type="number" id="countInStock" class="form-control" value="${data.countInStock}">
+        <input placeholder="Enter countInStock" type="number" id="countInStock" class="form-control" value="${inputs.countInStock}">
       </div>
       <div class="form-group">
         <label class="form-label" for="category">Category</label>
-        <input placeholder="Enter category" type="text" id="category" class="form-control" value="${data.category}">
+        <input placeholder="Enter category" type="text" id="category" class="form-control" value="${inputs.category}">
       </div>
       <div class="form-group">
         <label class="form-label" for="description">Description</label>
-        <input placeholder="Enter description" type="text" id="description" class="form-control" value="${data.description}">
+        <input placeholder="Enter description" type="text" id="description" class="form-control" value="${inputs.description}">
+      </div>
+      <div class="text-danger my-3 px-2" style="display:${
+        error ? 'block' : 'none'
+      }">
+        ${error ? error : ''}
       </div>
       <button type="submit" class="btn btn-primary">Update</button>
     `;
