33e976fe8b9e8de7760b8df2559aa8e91ad4088e
viveloper <ucsik1234@gmail.com>
2020-10-18 14:05:50 +0900

    updated readme.md

--- backend/server.js
+++ backend/server.js
@@ -26,29 +26,17 @@ app.use(express.json());
 
 app.use(cors());
 
+const __dirname = path.resolve();
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-
-app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
-
-const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
 
 app.use(notFound);
 app.use(errorHandler);
