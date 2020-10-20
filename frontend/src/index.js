4aac556d42287e355bccd8a33f05902f35b6837c
viveloper <ucsik1234@gmail.com>
2020-10-20 16:14:00 +0900

    added start log

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,6 @@
 import { renderComponent } from './modules/MyReact.js';
 import App from './App.js';
 
+console.log('Chova-Shop Start');
+
 renderComponent(App, null, document.querySelector('#app'));
